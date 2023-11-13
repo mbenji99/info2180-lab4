631ec5f6bdf0f8450863391dca88e9690ce34d60
Michael Benjamin <mbenji99@gmail.com>
2023-11-12 23:04:01 -0500

    Exercise 2 Attempted

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 document.getElementById('searchButton').addEventListener('click', function() {
-    fetch('http://localhost/info2180-lab4/superheroes.php') // Update the URL to use http://localhost
+    fetch('http://localhost:8888/info2180-lab4/superheroes.php') 
         .then(response => response.json())
         .then(data => {
-            let superheroesList = document.getElementById('superheroesList');
-            superheroesList.innerHTML = '';
-
-            data.forEach(superhero => {
-                let listItem = document.createElement('li');
-                listItem.textContent = superhero.alias;
-                superheroesList.appendChild(listItem);
-            });
+            const heroNames = data.map(superhero => superhero.alias).join('\n');
+            alert('List of Superheroes:\n' + heroNames);
         })
         .catch(error => {
             console.error('Error:', error);
--- superheroes.php
+++ superheroes.php
@@ -1,11 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:8888/info2180-lab4/superheroes.php"); 
+header("Content-Type: application/json"); 
 
 $superheroes = [
   [
       "id" => 1,
       "name" => "Steve Rogers",
       "alias" => "Captain America",
-      "biography" => "Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers.",
+      "biography" => "Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world`s mightiest heroes and the leader of the Avengers.",
   ],
   [
       "id" => 2,
@@ -17,7 +19,7 @@ $superheroes = [
       "id" => 3,
       "name" => "Peter Parker",
       "alias" => "Spiderman",
-      "biography" => "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
+      "biography" => "Bitten by a radioactive spider, Peter Parker`s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
   ],
   [
       "id" => 4,
@@ -29,25 +31,25 @@ $superheroes = [
       "id" => 5,
       "name" => "Natasha Romanov",
       "alias" => "Black Widow",
-      "biography" => "Despite super spy Natasha Romanoff’s checkered past, she’s become one of S.H.I.E.L.D.’s most deadly assassins and a frequent member of the Avengers.",
+      "biography" => "Despite super spy Natasha Romanoff`s checkered past, she`s become one of S.H.I.E.L.D.`s most deadly assassins and a frequent member of the Avengers.",
   ],
   [
       "id" => 6,
       "name" => "Bruce Banner",
       "alias" => "Hulk",
-      "biography" => "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he’s always been and the uncontrollable green monster powered by his rage.",
+      "biography" => "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he`s always been and the uncontrollable green monster powered by his rage.",
   ],
   [
       "id" => 7,
       "name" => "Clint Barton",
       "alias" => "Hawkeye",
-      "biography" => "A master marksman and longtime friend of Black Widow, Clint Barton serves as the Avengers’ amazing archer.",
+      "biography" => "A master marksman and longtime friend of Black Widow, Clint Barton serves as the Avengers` amazing archer.",
   ],
   [
       "id" => 8,
       "name" => "T'challa",
       "alias" => "Black Panther",
-      "biography" => "T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.",
+      "biography" => "T`Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.",
   ],
   [
       "id" => 9,
@@ -63,6 +65,7 @@ $superheroes = [
   ], 
 ];
 
+echo json_encode($superheroes);
 ?>
 
 <ul>
