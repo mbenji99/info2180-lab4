c0b897c50f3bf8191044af7f1a138d276a55c0b1
Michael Benjamin <mbenji99@gmail.com>
2023-11-12 23:52:48 -0500

    Exercise 3 Attempted

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 document.getElementById('searchButton').addEventListener('click', function() {
-    fetch('http://localhost:8888/info2180-lab4/superheroes.php') 
-        .then(response => response.json())
+    const searchQuery = document.getElementById('searchField').value.trim();
+    let url = 'http://localhost:8888/info2180-lab4/superheroes.php';
+
+    if (searchQuery) {
+        url += `?query=${encodeURIComponent(searchQuery)}`;
+    }
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
-            const heroNames = data.map(superhero => superhero.alias).join('\n');
-            alert('List of Superheroes:\n' + heroNames);
+            const resultDiv = document.getElementById('result');
+            if (data.length === 1) {
+                const superhero = data[0];
+                resultDiv.innerHTML = `
+                    <h3>${superhero.alias}</h3>
+                    <h4>${superhero.name}</h4>
+                    <p>${superhero.biography}</p>
+                `;
+            } else if (data.length > 1) {
+                const list = data.map(hero => `<li>${hero.alias}</li>`).join('');
+                resultDiv.innerHTML = `<ul>${list}</ul>`;
+            } else {
+                resultDiv.innerHTML = 'Superhero not found';
+            }
         })
         .catch(error => {
             console.error('Error:', error);
+            const resultDiv = document.getElementById('result');
+            resultDiv.innerHTML = 'An error occurred. Please try again.';
         });
 });
